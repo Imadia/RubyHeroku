c29ca22cbe96c48d9d0b8af7dca8aec143b407c6
Alejandra Diaz <ygthar@gmail.com>
2017-02-13 17:01:23 -0300

    2nd commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def x
   end
+
+  def save_user
+  	Registro.create(nombre: params[:nombre], email: params[:email], edad: params[:edad])
+  	redirect_to pages_x_path, notice: "El usuario ha sido guardado"
+  end
+
+  def see_users
+  	@users = Registro.all #guarda un arreglo de usuarios en users
+  end
+
 end
